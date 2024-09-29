ef007c332bdbe7b41299ffa1fbd1594387672acf
Daniel Barba <frdb103@gmail.com>
2024-09-29 03:37:31 -0400

    version 2 mobile friendly, game demo, more details

--- src/scenes/Contact.jsx
+++ src/scenes/Contact.jsx
@@ -112,12 +112,11 @@ export default function Contact() {
                                                     placeholder="Email" />
                                             </div>
                                         </div>
-                                        <div className="col-span-2">
-                                            <label className="text-gray-700" htmlFor="name">
-                                                <textarea id="comment" className=" shadow-xl flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                                                    placeholder="Enter your comment" name="comment" rows="5" cols="40">
-                                                </textarea>
-                                            </label>
+                                        <div className="col-span-2"> 
+                                                <input id="comment" className=" shadow-xl flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                                                    placeholder="Enter your comment" name="comment" >
+                                                </input>
+                                           
                                         </div>
                                     </>
                                     : <>
